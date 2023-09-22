a302cb4d07792dfe0628ad81a9b003ee6432198b
Jacob Humston <humstonjacob@gmail.com>
2023-09-21 23:53:43 -0400

    some small changes.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # FileView.php
+
 An easy to setup PHP script that allows you to display file directories for download and preview.
+
+# Docs
+
+Docs for version 1 can be found [here](docs/v1.md).
--- src/index.php
+++ src/index.php
@@ -1,23 +1,28 @@
 <?php
 
-// FileView.php
+declare(strict_types=1);
+
+// FileView.php (v1)
 // An easy to setup PHP script that allows you to display file directories for download and preview.
 // https://github.com/Lovely-Experiences/FileView.php
+// Docs: https://github.com/Lovely-Experiences/FileView.php/docs/v1.md
+
+// ---- Configuration ---- //
 
 // The path to your config file.
-$configFilePath = "./fvconfig.yaml";
+// This file does not need to be served to the web.
+$configFilePath = "./fvconfig.json";
 
-// The path to your contents folder.
-$contentsFilePath = "./fvcontents/";
+// The URL of your contents folder. 
+// This folder can be hosted remotely.
+// REMOTE URL: https://raw.githubusercontent.com/Lovely-Experiences/FileView.php/main/src/fvcontents/
+$contentsFilePath = "fvcontents/";
 
-?>
-
-<?php
+// --------- END --------- //
 
 // Load configuration.
-$config = yaml_parse_file($configFilePath);
+$config = json_decode(file_get_contents($configFilePath));
 global $config;
-
-echo $config->rootURL;
+global $contentsFilePath; // Making sure this is accessible everywhere.
 
 ?>
