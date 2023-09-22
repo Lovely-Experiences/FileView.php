a3f0356f4b17e4eb286abfb88ded7ad7c00f5a0d
Jacob Humston <humstonjacob@gmail.com>
2023-09-22 03:02:21 -0400

    Got the file icons ready as well as some other small changes.

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 An easy to setup PHP script that allows you to display files and directories for download and preview.
 
+Credits to [dmhendricks](https://github.com/dmhendricks) for [file-icon-vectors](https://github.com/dmhendricks/file-icon-vectors/tree/master).
+
 # Docs
 
 Docs for version 1 can be found [here](docs/v1.md).
--- src/index.php
+++ src/index.php
@@ -1,11 +1,11 @@
 <?php
 
-declare(strict_types=1);
-
 // FileView.php (v1)
 // An easy to setup PHP script that allows you to display file directories for download and preview.
 // https://github.com/Lovely-Experiences/FileView.php
 // Docs: https://github.com/Lovely-Experiences/FileView.php/docs/v1.md
+
+declare(strict_types=1);
 
 // ---- Configuration ---- //
 
@@ -13,16 +13,44 @@ declare(strict_types=1);
 // This file does not need to be served to the web.
 $configFilePath = "./fvconfig.json";
 
-// The URL of your contents folder. 
+// The URL of your contents folder.
 // This folder can be hosted remotely.
 // REMOTE URL: https://raw.githubusercontent.com/Lovely-Experiences/FileView.php/main/src/fvcontents/
-$contentsFilePath = "fvcontents/";
+$contentsFilePath = "./fvcontents/"; // You do not need to include the trailing slash.
 
 // --------- END --------- //
 
 // Load configuration.
 $config = json_decode(file_get_contents($configFilePath));
+
+// Create other needed variables.
+$files = new stdClass;
+
+// Modify variables.
+$contentsFilePath = rtrim($contentsFilePath, "/");
+
+// Set globals.
+global $contentsFilePath;
 global $config;
-global $contentsFilePath; // Making sure this is accessible everywhere.
+global $files;
+
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?php echo $config->pageTitle; ?>
+    </title>
+    <link rel="stylesheet" href="<?php echo $contentsFilePath; ?>/main.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
